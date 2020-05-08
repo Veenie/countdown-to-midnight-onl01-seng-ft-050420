940def41a5736c50b54baa9e3485043f7c547ec5
Raveen K Sharma <raveen08@gmail.com>
2020-05-08 02:24:38 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,10 @@
 #write your code here
 
 
-number = 10
-
 def countdown(number)
   while number >= 0
-    puts "#{number} SECOND(S)!"
-    number -= 1
+  puts "#{number} SECOND(S)!"
+  number -= 1
   if number == 0
       break
       return "HAPPY NEW YEAR!"
