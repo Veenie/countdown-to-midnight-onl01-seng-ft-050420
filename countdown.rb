342473e113f08dc08776b0e270bb9fd46af97b41
Raveen K Sharma <raveen08@gmail.com>
2020-05-08 03:17:00 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,10 +2,9 @@
 
 
 def countdown(number)
-  while number >= 0
+  while number > 0
   puts "#{number} SECOND(S)!"
   number -= 1
-  if number == 0
-    return "HAPPY NEW YEAR!"
-  end  
+  end
+  "HAPPY NEW YEAR!"
 end
